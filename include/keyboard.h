acb95c642c5881ad30a9daec296751590808e645
maginas2 <gjme@cesar.school>
2025-05-20 10:00:35 -0300

    Update keyboard.h
    Sem Comentários

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,9 +1,3 @@
-/**
- * keyboard.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
-*/
 
 #ifndef __KEYBOARD_H__
 #define __KEYBOARD_H__
@@ -16,4 +10,4 @@ int keyhit();
 
 int readch();
 
-#endif /* __KEYBOARD_H__ */
+#endif
